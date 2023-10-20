6e3b17ece77df2535485e03e6c069a252d37336b
Akat-Sireev <125801241+Akat-Sireev@users.noreply.github.com>
2023-10-20 18:14:30 +0300

    Initial commit

---
+++ testing.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double number = 1.1;
+    double roundedNumber = round(number); // Округляем число
+
+    std::cout << "Исходное число: " << number << std::endl;
+    std::cout << "Округленное число: " << roundedNumber << std::endl;
+
+    return 0;
+}
